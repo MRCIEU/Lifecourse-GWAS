4d01672a75b6d8c633868da654518a6b2c99a9e6
Gibran Hemani <explodecomputer@gmail.com>
2023-12-07 15:08:29 +0000

    hashing long alleles

--- resources/genotypes/variant_ids.r
+++ resources/genotypes/variant_ids.r
@@ -2,6 +2,7 @@ library(data.table)
 library(dplyr)
 library(here)
 library(glue)
+library(digest)
 
 # read in bim file
 
@@ -9,8 +10,23 @@ args <- commandArgs(T)
 bfile <- args[1]
 outfile <- args[2]
 
-# update orig bim
+compress_alleles <- function(a) {
+    i <- nchar(a) > 10
+    if(any(i)) {
+        a[i] <- sapply(a[i], \(x) digest(x, algo="murmur32"))
+    }
+    return(a)
+}
 
+# bim <- tribble(
+#     ~V1, ~V2, ~V3, ~V4, ~V5, ~V6,
+#     1, "rs234", 0, 1000, "G", "A",
+#     1, "rs234", 0, 1000, "GGGGGGGGGGGCCA", "A",
+#     1, "rs235", 0, 2000, "GGGGGGGGGGGCCAG", "A",
+#     1, "rs237", 0, 3000, "G", "T"
+# )
+
+# update orig bim
 bim <- data.table::fread(paste0(bfile, ".bim"))
 bim$switch <- bim$V5 > bim$V6
 temp <- bim$V5[bim$switch]
@@ -38,6 +54,6 @@ ch <- bim$V5 < bim$V6
 table(ch)
 stopifnot(all(ch))
 
-bim$V2 <- paste0(bim$V1, ":", bim$V4, "_", bim$V5, "_", bim$V6)
+bim$V2 <- paste0(bim$V1, ":", bim$V4, "_", compress_alleles(bim$V5), "_", compress_alleles(bim$V6))
 
 data.table::fwrite(bim, file=paste0(outfile, ".bim"), quote=FALSE, col.names=FALSE, sep=" ")
