0d63ba11c123f3feca1567c4a3f766be3a41f055
Gibran Hemani <explodecomputer@gmail.com>
2024-02-07 21:50:38 +0000

    updating documentation

--- 01-ancestry.sh
+++ 01-ancestry.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-## code to keep only a subset for testing purposes
-# head -n 1000 data_chr01.fam > temp1
-# tail -n 1000 data_chr01.fam > temp2
-# cat temp1 temp2 | awk '{print $1, $2}' > keep
-
-# for f in *bed
-# do
-#     echo $f
-#     bn=$(echo $f | sed 's/.bed//g')
-#     plink2 --bfile $bn --keep keep --make-bed --out subset/${bn}
-# done
-
-
-
 # strict stop if there are any errors
 set -e
 
