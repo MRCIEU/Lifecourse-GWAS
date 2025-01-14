a4790b7ea013cba6fdc6326c4fc508fd0ecf3afb
Gibran Hemani <explodecomputer@gmail.com>
2025-01-14 14:01:35 +0000

    fixed bug

--- test/covariates.r
+++ test/covariates.r
@@ -7,4 +7,4 @@ readRenviron(here("config.env"))
 
 a <- fread(file.path(Sys.getenv("genotype_processed_dir"), paste0("pcs.txt")), header=TRUE)
 
-b <- tibble(FID=a$FID, IID=a$IID, yob = sample(1920:1980, length(FID), replace=TRUE), sex=sample(1:2, length(FID), replace=TRUE), bp_med = rbinom(length(FID), 1, 0.2), choleterol_med= rbinom(length(FID), 1, 0.2), parity=sample(1:5, length(FID), replace=TRUE)
+b <- tibble(FID=a$FID, IID=a$IID, yob = sample(1920:1980, length(FID), replace=TRUE), sex=sample(1:2, length(FID), replace=TRUE), bp_med = rbinom(length(FID), 1, 0.2), choleterol_med= rbinom(length(FID), 1, 0.2), parity=sample(1:5, length(FID), replace=TRUE))
