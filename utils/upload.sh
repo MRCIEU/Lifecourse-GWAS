532b62fd5236f1cc713e684445efdbeff189d2e9
Gibran Hemani <g.hemani@bristol.ac.uk>
2025-03-07 11:48:10 +0000

    moving to using SAS tokens for upload

--- utils/upload.sh
+++ utils/upload.sh
@@ -17,7 +17,6 @@ if [ -z "$step" ]; then
 fi
 
 
-
 # Check if $step is in the list of steps
 if [[ ! " ${steps[@]} " =~ " ${step} " ]]; then
   echo "Step $step is not in the list of steps"
@@ -32,22 +31,9 @@ d=$(pwd)
 cd ${results_dir}
 md5sum -c ${cohort_name}_${step}.tar.gz.md5
 
-sftp_address="lcgwassftp.blob.core.windows.net"
-sftp_username_full="lcgwassftp.testcontainer.${sftp_username}"
 
-read -s -p "Ready to upload? Press enter to continue: " anykey
-echo ""
-read -s -p "Enter SFTP password: " mypassword
+${d}/bin/azcopy copy "${results_dir}/${cohort_name}_${step}.tar.gz" "https://lcgwassftp.blob.core.windows.net/analysts/${sftp_username}/${cohort_name}_${step}.tar.gz?${sas_token}"
 
-export SSHPASS=$mypassword
-${d}/bin/sshpass -e sftp -oBatchMode=no -b - ${sftp_username_full}@${sftp_address} << !
-   put ${results_dir}/${cohort_name}_${step}.tar.gz
-   put ${results_dir}/${cohort_name}_${step}.tar.gz.md5
-   bye
-!
-
-# This used to work and now doesn't - need to figure it out
-# curl -1 -v -k "sftp://${sftp_address}:22/${sftp_username}/${cohort_name}_${step}.tar.gz" --user "${sftp_username_full}:${mypassword}" -T "${results_dir}/${cohort_name}_${step}.tar.gz"
-# curl -1 -v -k "sftp://${sftp_address}:22/${sftp_username}/${cohort_name}_${step}.tar.gz.md5" --user "${sftp_username_full}:${mypassword}" -T "${results_dir}/${cohort_name}_${step}.tar.gz.md5"
+${d}/bin/azcopy copy "${results_dir}/${cohort_name}_${step}.tar.gz.md5" "https://lcgwassftp.blob.core.windows.net/analysts/${sftp_username}/${cohort_name}_${step}.tar.gz.md5?${sas_token}"
 
 echo "Successfully uploaded ${cohort_name}_${step}.tar.gz and ${cohort_name}_${step}.tar.gz.md5. Many thanks indeed!"
