0747922143bd2fbb9e526f740195ef09c47589c9
Gibran Hemani <explodecomputer@gmail.com>
2025-05-09 17:11:32 +0100

    updated 00a to allow pgen files

--- 00a-genotype-organisation.sh
+++ 00a-genotype-organisation.sh
@@ -22,8 +22,12 @@ echo "Performing check"
 grep -v "sas_token" config.env > ${results_dir}/00/config.env
 
 echo "Organise samples"
-Rscript resources/genotypes/organise_samples.r ${genotype_input_list} ${genotype_processed_dir}/sample_inclusion.txt ${sample_inclusion_list}
 
+if [[ ! -z $pgen_input ]]; then
+    Rscript resources/genotypes/organise_samples_pgen.r ${pgen_input}.psam ${genotype_processed_dir}/sample_inclusion.txt ${sample_inclusion_list}
+else
+    Rscript resources/genotypes/organise_samples.r ${genotype_input_list} ${genotype_processed_dir}/sample_inclusion.txt ${sample_inclusion_list}
+fi
 
 echo "Get list of pruned SNPs"
 if test -f "resources/genotypes/hm3_prune_${genome_build}.bed.gz"; then
@@ -36,75 +40,88 @@ else
     exit 1
 fi
 
-
-# Get list of bgen files
-echo "Checking genotype input list..."
-nchr=$(cat ${genotype_input_list} | grep -c '^')
-# Check nchr = 22 or 23
-if [[ $nchr -ne 22 && $nchr -ne 23 ]]; then
-    echo "Error: Expected 22 or 23 chromosomes, but found $nchr"
-    exit 1
-fi
-
-echo "Checking $nchr bgen files exist"
-for i in $(seq 1 $nchr)
-do
-    bgen=$(awk -v i=$i 'NR==i { print $1 }' ${genotype_input_list})
-    sample=$(awk -v i=$i 'NR==i { print $2 }' ${genotype_input_list})
-    if [ ! -f "${bgen}" ]; then
-        echo "${bgen} not found"
-        exit 1
-    fi
-
-    if [ ! -f "${sample}" ]; then
-        echo "${sample} not found"
-        exit 1
-    fi
-
-    if [[ ! $bgen == *.bgen ]]
-    then
-        echo "$bgen should be a bgen file ending in .bgen"
-        exit 1
-    fi
-
-    if [[ ! $sample == *.sample ]]
-    then
-        echo "$sample should be a sample file ending in .sample"
-        exit 1
-    fi
-done
-echo "All good!"
-
-mkdir -p ${genotype_processed_dir}/bgen_extract
-
-> ${genotype_processed_dir}/bgen_extract/mergelist
-
-for i in $(seq 1 $nchr)
-do
-    bgen=$(awk -v i=$i 'NR==i { print $1 }' ${genotype_input_list})
-    sample=$(awk -v i=$i 'NR==i { print $2 }' ${genotype_input_list})
+if [[ ! -z $pgen_input ]]; then
     ./bin/plink2 \
-        --bgen ${bgen} ref-first \
-        --sample ${sample} \
+        --pfile ${pgen_input} \
         --extract range ${prunefile} \
         --keep ${genotype_processed_dir}/sample_inclusion.txt \
         --make-bed \
-        --out ${genotype_processed_dir}/bgen_extract/$(basename ${bgen} .bgen) \
-        --threads ${env_threads}
-    echo "${genotype_processed_dir}/bgen_extract/$(basename ${bgen} .bgen)" >> ${genotype_processed_dir}/bgen_extract/mergelist
+        --threads ${env_threads} \
+        --max-alleles 2 \
+        --out ${genotype_processed_dir}/scratch/indep_th \
+        --maf 0.01 \
+        --maj-ref
+else
+    # Get list of bgen files
+    echo "Checking genotype input list..."
+    nchr=$(cat ${genotype_input_list} | grep -c '^')
+    # Check nchr = 22 or 23
+    if [[ $nchr -ne 22 && $nchr -ne 23 ]]; then
+        echo "Error: Expected 22 or 23 chromosomes, but found $nchr"
+        exit 1
+    fi
 
-    # rename any duplicates to be unique
-    Rscript resources/genotypes/dups_bim.r "${genotype_processed_dir}/bgen_extract/$(basename ${bgen} .bgen).bim"
-done
+    echo "Checking $nchr bgen files exist"
+    for i in $(seq 1 $nchr)
+    do
+        bgen=$(awk -v i=$i 'NR==i { print $1 }' ${genotype_input_list})
+        sample=$(awk -v i=$i 'NR==i { print $2 }' ${genotype_input_list})
+        if [ ! -f "${bgen}" ]; then
+            echo "${bgen} not found"
+            exit 1
+        fi
 
-./bin/plink2 \
-    --threads ${env_threads} \
-    --pmerge-list bfile ${genotype_processed_dir}/bgen_extract/mergelist \
-    --make-bed \
-    --max-alleles 2 \
-    --out ${genotype_processed_dir}/scratch/indep_th \
-    --maf 0.01 \
-    --maj-ref
+        if [ ! -f "${sample}" ]; then
+            echo "${sample} not found"
+            exit 1
+        fi
+
+        if [[ ! $bgen == *.bgen ]]
+        then
+            echo "$bgen should be a bgen file ending in .bgen"
+            exit 1
+        fi
+
+        if [[ ! $sample == *.sample ]]
+        then
+            echo "$sample should be a sample file ending in .sample"
+            exit 1
+        fi
+    done
+    echo "All good!"
+
+    mkdir -p ${genotype_processed_dir}/bgen_extract
+
+    > ${genotype_processed_dir}/bgen_extract/mergelist
+
+    for i in $(seq 1 $nchr)
+    do
+        bgen=$(awk -v i=$i 'NR==i { print $1 }' ${genotype_input_list})
+        sample=$(awk -v i=$i 'NR==i { print $2 }' ${genotype_input_list})
+        ./bin/plink2 \
+            --bgen ${bgen} ref-first \
+            --sample ${sample} \
+            --extract range ${prunefile} \
+            --keep ${genotype_processed_dir}/sample_inclusion.txt \
+            --make-bed \
+            --out ${genotype_processed_dir}/bgen_extract/$(basename ${bgen} .bgen) \
+            --threads ${env_threads}
+        echo "${genotype_processed_dir}/bgen_extract/$(basename ${bgen} .bgen)" >> ${genotype_processed_dir}/bgen_extract/mergelist
+
+        # rename any duplicates to be unique
+        Rscript resources/genotypes/dups_bim.r "${genotype_processed_dir}/bgen_extract/$(basename ${bgen} .bgen).bim"
+    done
+
+    ./bin/plink2 \
+        --threads ${env_threads} \
+        --pmerge-list bfile ${genotype_processed_dir}/bgen_extract/mergelist \
+        --make-bed \
+        --max-alleles 2 \
+        --out ${genotype_processed_dir}/scratch/indep_th \
+        --maf 0.01 \
+        --maj-ref
+fi
+
 
 thfile="${genotype_processed_dir}/scratch/th.txt"
 gunzip -c resources/genotypes/tophitsnps_${genome_build}.bed.gz > ${thfile}
