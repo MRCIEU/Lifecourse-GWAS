6bce5192bfb349882f8615182122825d3ab3c883
Gibran Hemani <explodecomputer@gmail.com>
2025-05-09 22:45:20 +0100

    allowing pgen files in containers

--- utils/run_docker.sh
+++ utils/run_docker.sh
@@ -7,15 +7,32 @@ source config.env
 # docker build -t mrcieu/lifecourse-gwas:latest .
 # docker pull mrcieu/lifecourse-gwas:latest
 
-genotype_input_dir=$(head -n 1 geno_input.txt | awk '{ print $1 }' | xargs -I {} dirname {})
+if [[ ! -z $pgen_input ]]; then
+    echo "Using pgen input"
+    # Check that $pgen_input is set
+    pgen_input_dir=$(dirname $pgen_input)
 
-docker run \
-    -v "${PWD}/config.env:/project/config.env:ro" \
-    -v "${PWD}/geno_input.txt:/project/geno_input.txt:ro" \
-    -v ${phenotype_input_dir}:${phenotype_input_dir} \
-    -v ${phenotype_processed_dir}:${phenotype_processed_dir} \
-    -v ${genotype_input_dir}:${genotype_input_dir} \
-    -v ${genotype_processed_dir}:${genotype_processed_dir} \
-    -v ${results_dir}:${results_dir} \
-    mrcieu/lifecourse-gwas:latest \
-    "$@"
+    docker run \
+        -v "${PWD}/config.env:/project/config.env:ro" \
+        -v ${phenotype_input_dir}:${phenotype_input_dir} \
+        -v ${phenotype_processed_dir}:${phenotype_processed_dir} \
+        -v ${pgen_input_dir}:${pgen_input_dir} \
+        -v ${genotype_processed_dir}:${genotype_processed_dir} \
+        -v ${results_dir}:${results_dir} \
+        mrcieu/lifecourse-gwas:latest \
+        "$@"
+else
+    echo "Using bgen input"
+    genotype_input_dir=$(head -n 1 geno_input.txt | awk '{ print $1 }' | xargs -I {} dirname {})
+
+    docker run \
+        -v "${PWD}/config.env:/project/config.env:ro" \
+        -v "${PWD}/geno_input.txt:/project/geno_input.txt:ro" \
+        -v ${phenotype_input_dir}:${phenotype_input_dir} \
+        -v ${phenotype_processed_dir}:${phenotype_processed_dir} \
+        -v ${genotype_input_dir}:${genotype_input_dir} \
+        -v ${genotype_processed_dir}:${genotype_processed_dir} \
+        -v ${results_dir}:${results_dir} \
+        mrcieu/lifecourse-gwas:latest \
+        "$@"
+fi
