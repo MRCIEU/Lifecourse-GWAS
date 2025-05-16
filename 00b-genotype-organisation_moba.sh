37fd8560190561c49ad161abe798ebe202fc8313
gracemarionpower <88652994+gracemarionpower@users.noreply.github.com>
2025-05-16 13:27:41 +0200

    Update 00b-genotype-organisation_moba.sh

--- 00b-genotype-organisation_moba.sh
+++ 00b-genotype-organisation_moba.sh
@@ -6,9 +6,13 @@ set -e
 # get environmental variables
 source config.env
 ./bin/gcta64 \
-    --bfile /home/grace.power/archive/moba/geno/MobaPsychgenReleaseMarch23/MoBaPsychGen_v1/MoBaPsychGen_v1-ec-eur-batch-basic-qc \
+    --bed /home/grace.power/archive/moba/geno/MobaPsychgenReleaseMarch23/MoBaPsychGen_v1/MoBaPsychGen_v1-ec-eur-batch-basic-qc.bed \
+    --bim /home/grace.power/archive/moba/geno/MobaPsychgenReleaseMarch23/MoBaPsychGen_v1/MoBaPsychGen_v1-ec-eur-batch-basic-qc.bim \
+    --fam /home/grace.power/work/gpower/data/lifecourse_gwas_data_curation/geno/MoBaPsychGen_v1-ec-eur-batch-basic-qc.fid_replaced.fam \   
     --pheno ${genotype_processed_dir}/scratch/phenrand.txt \
     --fastGWA-lr \
+    --extract ${genotype_processed_dir}/variant_inclusion.txt \
+    --keep ${genotype_processed_dir}/sample_inclusion.txt \
     --thread-num ${env_threads} \
     --geno 0.1 \
     --maf ${env_minmaf} \
@@ -19,3 +23,4 @@ Rscript resources/genotypes/nullgwas.r \
     ${results_dir}/00
  
 echo "Successfully summarised and filtered variants"
+ 
