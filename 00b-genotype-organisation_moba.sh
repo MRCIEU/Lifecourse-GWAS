ec92e52a2c65fd8177ca6e956e57f612050ea26c
gracemarionpower <88652994+gracemarionpower@users.noreply.github.com>
2025-05-16 13:02:07 +0200

    Create 00b-genotype-organisation_moba.sh

---
+++ 00b-genotype-organisation_moba.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+ 
+# strict stop if there are any errors
+set -e
+ 
+# get environmental variables
+source config.env
+./bin/gcta64 \
+    --bfile /home/grace.power/archive/moba/geno/MobaPsychgenReleaseMarch23/MoBaPsychGen_v1/MoBaPsychGen_v1-ec-eur-batch-basic-qc \
+    --pheno ${genotype_processed_dir}/scratch/phenrand.txt \
+    --fastGWA-lr \
+    --extract ${genotype_processed_dir}/variant_inclusion.txt \
+    --keep ${genotype_processed_dir}/sample_inclusion.txt \
+    --thread-num ${env_threads} \
+    --geno 0.1 \
+    --maf ${env_minmaf} \
+    --out ${genotype_processed_dir}/scratch/phenrand
+ 
+Rscript resources/genotypes/nullgwas.r \
+    ${genotype_processed_dir}/scratch/phenrand.fastGWA \
+    ${results_dir}/00
+ 
+echo "Successfully summarised and filtered variants"
