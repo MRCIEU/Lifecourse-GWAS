f2ef46400523611eaa4c0ac4501f4378677347f7
Gibran Hemani <explodecomputer@gmail.com>
2025-07-30 22:25:32 +0100

    removing print

--- resources/genotypes/rand.r
+++ resources/genotypes/rand.r
@@ -6,5 +6,4 @@ args <- commandArgs(T)
 fam <- fread(args[1], header=FALSE) %>% as_tibble()
 fam <- dplyr::select(fam, V1, V2) %>%
     mutate(phen=rnorm(n()))
-fam
 fwrite(fam, args[2], row=F, col=F, qu=F, sep=" ")
