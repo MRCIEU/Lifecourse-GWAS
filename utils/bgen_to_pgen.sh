e770cfda571d35d4dba4bfdf7942f69d959ef71b
Gibran Hemani <explodecomputer@gmail.com>
2025-09-23 11:09:10 +0100

    utils/bgen_to_pgen.sh

--- utils/bgen_to_pgen.sh
+++ utils/bgen_to_pgen.sh
@@ -8,7 +8,7 @@ pgendir=$1
 inclusion_file=$2
 
 if [ -z $pgendir ]; then
-    echo "Usage: ./bgen_to_pgen.sh <pgendir>" <sample inclusion file>
+    echo "Usage: ./bgen_to_pgen.sh <pgendir> <sample inclusion file>"
     exit 1
 fi
 
@@ -26,22 +26,24 @@ do
     bn=$(basename $bgen .bgen)
     dn=$(dirname $bgen)
 
-    ./bin/plink2 --bgen ${bgen} ref-first --sample ${sample} --make-pgen --out ${pgendir}/${bn} --threads ${env_threads} --maf ${env_maf} --keep ${inclusion_file}
+    ./bin/plink2 --bgen ${bgen} ref-first --sample ${sample} --make-pgen --out ${pgendir}/${bn} --threads ${env_threads} --maf ${env_minmaf} --keep ${inclusion_file} --geno ${env_miss} --mind ${env_imiss}
     echo "${pgendir}/${bn}" >> $tf
 done
 
 echo "Merging chromosomes"
 
-./bin/plink2 --pfile ${pgendir}/chr1 --merge-list $tf --make-pgen --out ${pgendir}/allchr --threads ${env_threads}
+./bin/plink2 --pmerge-list $tf --make-pgen --out ${pgendir}/allchr --threads ${env_threads}
 
-echo "Removing per-chromosome pgen files"
+echo "Removing 'chr' prefix from pvar file"
 
-for i in $(seq 1 ${nchr})
-do
-    rm ${pgendir}/${bn}.pgen
-    rm ${pgendir}/${bn}.pvar
-    rm ${pgendir}/${bn}.psam
-done
+cp ${pgendir}/allchr.pvar ${pgendir}/allchr.pvar.original
+sed -i 's/chr//g' ${pgendir}/allchr.pvar
+
+echo "Chromosomes in original pvar file"
+awk '{ print $1 }' ${pgendir}/allchr.pvar.original | sort | uniq -c 
+
+echo "Chromosomes in new pvar file"
+awk '{ print $1 }' ${pgendir}/allchr.pvar | sort | uniq -c 
 
 echo "New pgen dataset: ${pgendir}/allchr.pgen, ${pgendir}/allchr.pvar, ${pgendir}/allchr.psam"
 echo "Successfully converted to bgen files to pgen"
