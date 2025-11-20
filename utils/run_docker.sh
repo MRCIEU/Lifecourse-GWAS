1b4663c1a4e0d8b1db13a4e310655bf841087a34
Gibran Hemani <g.hemani@bristol.ac.uk>
2025-11-20 17:13:25 +0000

    flag when in container

--- utils/run_docker.sh
+++ utils/run_docker.sh
@@ -13,6 +13,7 @@ if [[ ! -z $pgen_input ]]; then
     pgen_input_dir=$(dirname $pgen_input)
 
     docker run \
+        --env APPTAINER="true" \
         -v "${PWD}/config.env:/project/config.env:ro" \
         -v ${phenotype_input_dir}:${phenotype_input_dir} \
         -v ${phenotype_processed_dir}:${phenotype_processed_dir} \
@@ -26,6 +27,7 @@ else
     genotype_input_dir=$(head -n 1 geno_input.txt | awk '{ print $1 }' | xargs -I {} dirname {})
 
     docker run \
+        --env APPTAINER="true" \
         -v "${PWD}/config.env:/project/config.env:ro" \
         -v "${PWD}/geno_input.txt:/project/geno_input.txt:ro" \
         -v ${phenotype_input_dir}:${phenotype_input_dir} \
